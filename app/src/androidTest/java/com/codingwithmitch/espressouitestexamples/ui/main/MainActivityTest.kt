fa33b014a475a27db41641cb3fefe3a810844a86
Chydee <desmondmuqsit@yahoo.com>
2020-03-01 08:36:43 +0100

    added activity rule

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
@@ -1,36 +1,20 @@
 package com.codingwithmitch.espressouitestexamples.ui.main
 
-import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.codingwithmitch.espressouitestexamples.R
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
-    @Test
-    fun testActivityInView() {
-        //This creates an activity inside this test Function
-        val activityScenerio = ActivityScenario.launch(MainActivity::class.java)
 
-        onView(withId(R.id.main)).check(matches(isDisplayed()))
-    }
+    @get: Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun testVisibilityOfTitleAndNextButton() {
-        val activityScenerioRule = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()))
-        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed()))//Method 1
-        onView(withId(R.id.button_next_activity)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))//Method 2
-    }
+    fun test_navSecondaryActivity() {
 
-    @Test
-    fun test_is_title_text_displayed() {
-        val activityScenerioRule = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity)))
     }
 }
 
